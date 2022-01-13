9da54b63bec1f810af462ed425dc15ab4e9209b2
josegazano732 <josegazano7@outlook.com>
2022-01-13 15:51:34 -0300

    JLG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-AP';
+  title = 'proyecto-angular Grupo 76';
+
+  inputText = document.getElementById("#inputTarea");
+  tareas: any[] = []; // Creamos un array de tareas para trerlod aqui.
+
+
+// Creamos el metodo dentro de la clasee AppComponent.
+  crearTarea(tarea: string/*parametro que se le pasa es el inputTarea.value*/, $event: any) {
+    $event.preventDefault(); // Para prevenir 
+    //console.log(tarea);
+
+    //agregamos una tarea en nuestro array tareas.
+    this.tareas.push({
+      id: Date.now(),
+      texto: tarea,
+    });
+
+    
+
+
+    console.log(this.inputText);
+
+
+  }
+
+  eliminarTarea(id:number){
+   // console.log(id);
+    this.tareas = this.tareas.filter(tarea => tarea.id !== id);
+    
+  }
 }
+
+//metodo para crear tareas
+
